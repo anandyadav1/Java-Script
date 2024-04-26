dfaae0a6373ba017398c4d7f5a4af3f6689b985a
Anand Kumar yadav <ay4799958@gmail.com>
2024-04-26 14:18:34 +0530

    Initial Code

---
+++ String-Method/Practice set/app1.js
@@ -0,0 +1,7 @@
+let msg="help!";
+console.log(msg.trim().toUpperCase());
+let name="ApnaCollege";
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("apna","Our"));
+console.log(name.replace("College","I","t"));
---
+++ String-Method/app.js
@@ -0,0 +1,48 @@
+// let msg="  Hello  ";
+// console.log(msg.trim());
+// console.log(msg);
+
+// let password=prompt("Set your password");
+// let newPass=password.trim();
+// console.log(newPass); 
+
+// //Lower&Upper Case
+// let name="AnandYadav";
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// //Argument
+// console.log(name.method(arg)); 
+
+//indexOf String method
+let str="ILoveCoding";
+// console.log(str.indexOf("I")); 
+// console.log(str.indexOf("ILove")); 
+// console.log(str.indexOf("Coding")); 
+// console.log(str.indexOf("c")); 
+// console.log(str.indexOf("i")); 
+
+//Method Chaining
+// let msg="   anand G L Bajaj   ";
+// console.log("Current String: ",msg);
+// console.log("afer triim: ",msg.trim());
+// console.log("afer Uppercase: ",msg.toUpperCase());
+// console.log("print by method chaining");
+// console.log(msg.trim().toUpperCase());
+
+//Slice method in string
+// let str="ILoveCoding";
+// console.log(str.slice(1))
+// console.log(str.slice(2))
+// console.log(str.slice(3))
+// console.log(str.slice(4))
+// console.log(str.slice(1,6))
+// console.log(str.slice(5))
+
+//Replace method in string
+// console.log(str.replace("Love","Coming"));
+// console.log(str.replace("Love","HTMLAudioElement"));
+// console.log(str.replace("ve","Comingandgoing"));
+// console.log(str.replace("e","I Love You"));
+
+//Repeat method in string
+console.log(str.repeat(5));
